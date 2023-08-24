326cbf7769ede2a030035200b641480e668a2dd3
unknown <sy.diakaridia@odc.local>
2023-08-24 15:16:25 +0000

    jai fini de faire tout les champs

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-detail',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent {
+
+  confirmBox() {
+    Swal.fire({
+      title: 'Voulez-vous vraiment supprimer?',
+      text: 'Vous ne pourriez plus le récupérer!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Oui Supprimez-le',
+      cancelButtonText: 'Non, garde-le',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire('Supprimé!', 'Médicament supprimé avec succès.', 'success');
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire('Annulé', 'Médicament n\'a pas été modifier.)', 'error');
+      }
+    }); 
+  }
+
 
 }
