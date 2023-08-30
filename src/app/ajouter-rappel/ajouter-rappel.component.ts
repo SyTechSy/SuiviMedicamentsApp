0fda7577bd5e92e7ea80dc2b912fd64e521e2bc1
touredri <dcompte223@gmail.com>
2023-08-30 16:43:11 +0000

    rappel fonctionne

--- src/app/ajouter-rappel/ajouter-rappel.component.ts
+++ src/app/ajouter-rappel/ajouter-rappel.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Rappel } from "../models/rappel.model";
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RappelServiceService } from "../rappel-service.service";
 import { MedicamentService } from "../mon-service/medicament.service";
 import { HttpClient } from "@angular/common/http";
+import Imedicament from "../models/Imedicament";
+import {Observable} from "rxjs";
+
 
 @Component({
   selector: 'app-ajouter-rappel',
@@ -15,18 +18,25 @@ export class AjouterRappelComponent {
     public rappelservice: RappelServiceService,
     private route: ActivatedRoute,
     private medicamentService: MedicamentService,
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private router: Router
   ) {}
   id = this.route.snapshot.params['id'];
-
-  medicament = this.medicamentService.getMedicament(this.id);
-
-
-
+  medicament: Observable<Imedicament> | undefined;
 
   getRappelLength(): number {
     const rappelArray = this.rappelservice.getRappel() || [];
     return rappelArray.length + 1;
   }
-  nouveauRappel : Rappel = new Rappel(this.getRappelLength(), new Date(), new Date(), this.medicament, '');
+  nouveauRappel : Rappel = new Rappel(this.getRappelLength(), '', '', '');
+
+  createSubmit() {
+    this.medicamentService.getMedicament(this.id).subscribe((medicament: Imedicament) => {
+      this.nouveauRappel.medicamentId = medicament.id;
+      medicament.prochaine_prise = this.nouveauRappel.heure_de_signale.toString();
+      this.rappelservice.addRappel(this.nouveauRappel);
+      this.medicamentService.updateMedicament(medicament, this.id).subscribe();
+      this.router.navigate(['medicaments/detail/', this.id]);
+    });
+    }
 }
